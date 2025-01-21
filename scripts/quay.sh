f0307714e48e5248466c08a8a0a7ffa5e349bace
Rob Seward (SOM) <rseward@bluestone-consulting.com>
2025-01-21 16:40:45 -0500

    Adjust the quay build github action scripts.

--- scripts/quay.sh
+++ scripts/quay.sh
@@ -1,10 +1,23 @@
 set -e
 
-app="${1}"
-version="${2}"
+#app="${1}"
+#version="${2}"
+RELEASE_TYPE=$1
 
 echo "Logging into Quay..."
 echo "${QUAY_PASSWORD}" | docker login -u "${QUAY_USERNAME}" --password-stdin quay.io
 
-docker tag smartcitiesdata/$app:$version quay.io/urbanos/$app:$version
-docker push quay.io/urbanos/$app:$version
+if [[ $RELEASE_TYPE == "release" ]]; then
+    export TAGGED_IMAGE="urbanos/discovery_ui:${GITHUB_BRANCH}"
+elif [[ $RELEASE_TYPE == "master" ]]; then
+    export TAGGED_IMAGE="urbanos/discovery_ui:development"
+else
+    echo "Branch should not be pushed to Dockerhub"
+    exit 0
+fi
+
+docker tag discovery_ui:build "quay.io/${TAGGED_IMAGE}"
+docker push "quay.io/${TAGGED_IMAGE}"
+
+#docker tag smartcitiesdata/$app:$version quay.io/urbanos/$app:$version
+#docker push quay.io/urbanos/$app:$version
