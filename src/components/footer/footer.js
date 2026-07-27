d94032cb06bc27fbfaa3ac30d03b66e5476634fe
smithc197 <smithc197@michigan.gov>
2026-07-27 11:28:45 -0400

    Added version number to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import './footer.scss'
-
+const { version } = require('../../../package.json')
 const Footer = () => {
   const rightLinks = JSON.parse(window.FOOTER_RIGHT_LINKS)
   const renderLink = ({ url, linkText }) => <a key={linkText} className='link' href={url}> {linkText}</a>
@@ -11,6 +11,7 @@ const Footer = () => {
           {window.FOOTER_LEFT_SIDE_TEXT && <li key='left' className='left-side-text'>
             {renderLink({ url: window.FOOTER_LEFT_SIDE_LINK, linkText: window.FOOTER_LEFT_SIDE_TEXT })}
           </li>}
+        <li>Version {version}</li>
         </ul>
         <ul key='right' className='links'>
           {rightLinks.map(link => <li key={link.linkText}>{renderLink(link)}</li>)}
